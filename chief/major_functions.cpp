4e1f82cd06e3805519c285d1a6c7f79692fc7bd5
itzandroidtab <koen@itzandroidtab.nl>
2025-12-21 22:25:25 +0100

    added mj_create implementation

--- chief/major_functions.cpp
+++ chief/major_functions.cpp
@@ -1,9 +1,229 @@
+#include <limits.h>
+
 #include "major_functions.hpp"
 #include "spinlock.hpp"
 #include "device_extension.hpp"
 
+static bool delete_is_not_pending(__in struct _DEVICE_OBJECT *DeviceObject) {
+    chief_device_extension* dev_ext = (chief_device_extension*)DeviceObject->DeviceExtension;
+    
+    // return if the device is not being removed or ejected and has a configuration descriptor
+    return !dev_ext->is_removing && dev_ext->has_config_desc && !dev_ext->is_removing && !dev_ext->someflag_22;
+}
+
+static ULONG get_pipe_from_unicode_str(_DEVICE_OBJECT* DeviceObject, _UNICODE_STRING* FileName) {
+    // get the device extension
+    chief_device_extension* dev_ext = (chief_device_extension*)DeviceObject->DeviceExtension;
+    
+    // get the length in characters
+    const unsigned int length_in_chars = FileName->Length / sizeof(wchar_t);
+
+    // search backwards for the last digit in the file name. Then we will read all the 
+    // digits until we hit a non-digit character. This is the offset of the pipe in the
+    // allocated pipes array.
+    // example: "Device\ChiefUSB\Pipe13" -> we want to read '13' as the pipe index
+    int index;
+    for (index = (length_in_chars - 1); index >= 0; index--) {
+        // get the current character
+        const wchar_t ch = FileName->Buffer[index];
+
+        // check if its a digit
+        if (ch < L'0' || ch > L'9') {
+            break;
+        }
+    }
+
+    // check if we found any digits
+    if (!index) {
+        return ULONG_MAX;
+    }
+
+    int pipe_offset = 0;
+    int multiplier = 1;
+    
+    // convert the digits to a number. Convert backwards from index to 0
+    for (; index >= 0; index--) {
+        // get the current character
+        const wchar_t ch = FileName->Buffer[index];
+
+        // check if its a digit. If not we have reached the end of the number
+        if (ch < L'0' || ch > L'9') {
+            break;
+        }
+
+        // convert to number
+        const int digit = ch - L'0';
+
+        // add the digit to the offset
+        pipe_offset += (digit * multiplier);
+        multiplier *= 10;
+    }
+
+    return pipe_offset;
+}
+
+void set_power_complete(PDEVICE_OBJECT DeviceObject, UCHAR MinorFunction, POWER_STATE PowerState, PVOID Context, PIO_STATUS_BLOCK IoStatus) {
+    // TODO: why are they not using the DeviceObject parameter directly? No need
+    // for a context here
+    PDEVICE_OBJECT device_object = (PDEVICE_OBJECT)Context;
+
+    // get the device extension
+    chief_device_extension* dev_ext = (chief_device_extension*)device_object->DeviceExtension;
+
+    if (PowerState.SystemState < dev_ext->powerstate2.SystemState) {
+        // set the event to signal the power request is complete
+        KeSetEvent(&dev_ext->event3, EVENT_INCREMENT, false);
+    }
+
+    // release the spinlock
+    spinlock_release(device_object);
+}
+
+NTSTATUS change_power_state_impl(_DEVICE_OBJECT* DeviceObject, const POWER_STATE state) {
+    // get the device extension
+    chief_device_extension* dev_ext = (chief_device_extension*)DeviceObject->DeviceExtension;
+
+    // check if we are already in the requested state
+    if (dev_ext->powerstate0.SystemState == state.DeviceState) {
+        return STATUS_SUCCESS;
+    }
+
+    // acquire the spinlock
+    spinlock_acquire(DeviceObject);
+
+    // mark the power request as busy
+    dev_ext->power_request_busy = true;
+
+    // send a IRP_MN_SET_POWER request to the driver
+    NTSTATUS status = PoRequestPowerIrp(
+        dev_ext->physicalDeviceObject,
+        IRP_MN_SET_POWER,
+        state,
+        set_power_complete,
+        DeviceObject,
+        nullptr
+    );
+
+    // check if we have a pending status
+    if (status == STATUS_PENDING) {
+        // check if we need to wait for the request to complete
+        if (state.SystemState < dev_ext->powerstate2.SystemState) {
+            KeWaitForSingleObject(
+                &dev_ext->event3,
+                Suspended,
+                KernelMode,
+                false,
+                nullptr
+            );
+        }
+
+        status = STATUS_SUCCESS;
+
+        // TODO: if the status is not pending this variable will never 
+        // be cleared. This doesnt look correct.
+        dev_ext->power_request_busy = false;
+    }
+
+    return status;
+}
+
+NTSTATUS change_power_state(_DEVICE_OBJECT* DeviceObject, const bool a2) {
+    // check if we have a delete pending
+    if (!delete_is_not_pending(DeviceObject)) {
+        return STATUS_DELETE_PENDING;
+    }
+
+    // get the device extension
+    chief_device_extension* dev_ext = (chief_device_extension*)DeviceObject->DeviceExtension;
+
+    // check if we have a power irq pening or we have a power request busy
+    if (dev_ext->irp0 || dev_ext->power_request_busy) {
+        return STATUS_SUCCESS;
+    }
+
+    // TODO: figure out what the remainder of this function does. It doesnt
+    // make too much sense
+    if (a2) {
+        if (dev_ext->lock_count) {
+            return STATUS_SUCCESS;
+        }
+    }
+    else if (!dev_ext->lock_count) {
+        return STATUS_SUCCESS;
+    }
+
+    // get the power state
+    const POWER_STATE state = dev_ext->powerstate2;
+
+    // check if we are currently in a working, unspecified or 
+    // hibernate state (we only pass here if we are in a 
+    // sleeping1/2/3 or shutdown state)
+    if (state.SystemState == PowerSystemWorking || 
+        state.SystemState == PowerSystemUnspecified ||
+        state.SystemState == PowerSystemHibernate) 
+    {
+        return STATUS_SUCCESS;
+    } 
+
+    if (!a2) {
+        return change_power_state_impl(DeviceObject, {PowerSystemWorking});
+    }
+
+    return change_power_state_impl(DeviceObject, state);
+}
+
 NTSTATUS mj_create(__in struct _DEVICE_OBJECT *DeviceObject, __inout struct _IRP *Irp) {
-    return STATUS_SUCCESS;
+    // get the device extension
+    chief_device_extension* dev_ext = (chief_device_extension*)DeviceObject->DeviceExtension;
+
+    // aquire the spinlock
+    spinlock_acquire(DeviceObject);
+
+    NTSTATUS status = STATUS_SUCCESS;
+
+    // check if the device is being removed or ejected
+    if (!delete_is_not_pending(DeviceObject)) {
+        status = STATUS_DELETE_PENDING;
+    }
+    else {
+        // we are not being deleted. Get the current file object in the irp
+        PFILE_OBJECT file = IoGetCurrentIrpStackLocation(Irp)->FileObject;
+        file->FsContext = nullptr;
+    
+        // check if we have a file name
+        if (file->FileName.Length) {
+            // get the pipe index from the file name
+            ULONG pipe_index = get_pipe_from_unicode_str(DeviceObject, &file->FileName);
+
+            // check if we got a valid pipe index
+            if (pipe_index >= dev_ext->usb_interface_info->NumberOfPipes) {
+                status = STATUS_INVALID_PARAMETER;
+            }
+            else {
+                // store the pipe information in the fs context
+                file->FsContext = (void*)&dev_ext->usb_interface_info->Pipes[pipe_index];
+
+                // mark the pipe as allocated
+                dev_ext->allocated_pipes[pipe_index] = true;
+
+                // increment the interlocked value
+                InterlockedIncrement(&dev_ext->lock_count);
+
+                // change the power state
+                change_power_state(DeviceObject, false);
+            }
+        }
+    }
+
+    Irp->IoStatus.Status = status;
+    Irp->IoStatus.Information = 0;
+
+    IofCompleteRequest(Irp, IO_NO_INCREMENT);
+
+    // release the spinlock
+    spinlock_release(DeviceObject);
+
+    return status;
 }
 
 NTSTATUS mj_close(__in struct _DEVICE_OBJECT *DeviceObject, __inout struct _IRP *Irp) {
